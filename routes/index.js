08bc4825c235ee646546bd7c33b17d5f8601909a
Ronal0007-dev <kallagiga@gmail.com>
2025-11-21 08:17:27 +0300

    21th Nov

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,20 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express.Router();
+
+//parse application/json
+app.use(bodyParser.json());
+//parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+app.get('/', function(req, res, next) {
   res.render('index', { title: 'HOME | PAST' });
 });
 
-router.get('/login', function(req, res, next) {
+app.get('/login', function(req, res, next) {
   res.render('login', { title: 'LOGIN | PAST' });
 });
 
-module.exports = router;
+module.exports = app;
